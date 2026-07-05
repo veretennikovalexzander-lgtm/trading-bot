dd9555b2b86c99459341285d0a727da149726494
Boss <veretennikov.alexzander@gmail.com>
2026-07-05 22:48:34 +0300

    Add raw kline received log

--- src/main.py
+++ src/main.py
@@ -353,6 +353,7 @@ class BotController:
             self.watchdog.record_ws_disconnect()
 
     def _on_kline(self, msg: dict):
+        logger.info(f"Kline received: {symbol} closed={is_closed} price={candle["close"]}")
         if not self.running:
             return
         data = msg.get("data", {})
